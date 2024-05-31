9a6d521a8001f655a9de8353275f810c0a8e2460
lee.chaebeen <cblee@cnrres.com>
2024-05-31 22:59:14 +0900

    BFS_027

---
+++ src/basic/search/BFS/미로탐색하기_027.java
@@ -0,0 +1,67 @@
+package basic.search.BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2178
+public class 미로탐색하기_027 {
+
+    // 상하좌우를 탐색하기 위한 배열 선언하기
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static boolean[][] visited;
+    static int[][] A;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        // 입력값 초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new int[N][M];
+        visited = new boolean[N][M];
+
+        for(int i = 0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for(int j=0; j<M; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j, j +1));
+            }
+        }
+
+        BFS(0,0);
+        System.out.println(A[N-1][M-1]); // 0부터 인덱스 시작해서 -1 해줌
+    }
+
+    static void BFS(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {i, j});
+        visited[i][j] = true;
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll(); // 현재 좌표
+            // 상하좌우 확인
+            for(int k=0; k < 4; k++) {
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+                // 좌표 유효성
+                if( x >= 0 && y >= 0 && x < N && y < M) {
+                    // 갈 수 있는 칸 && 방문 여부 검사
+                    if(A[x][y] != 0 && !visited[x][y]) {
+                        visited[x][y] = true;
+                        A[x][y] = A[now[0]][now[1]] + 1; // 깊이 업데이트 ==> 현재 좌표값의 depth + 1 == 몇번 BFS 를 반복했는지
+                        queue.add(new int[] {x, y}); // 다음 기준 좌표
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
